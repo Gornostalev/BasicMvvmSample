7dc927ac8cca221cf2de4a0a6784c4ccab4fbdfa
Gornostalev <egornostalev@yandex.ru>
2025-03-06 14:33:27 +0500

    add reactive view

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,5 +5,8 @@ public partial class MainWindowViewModel : ViewModelBase
     public string Greeting { get; } = "Welcome to Avalonia!";
 
     public SimpleViewModel SimpleViewModel { get; } = new SimpleViewModel();
+
+    // Наша новая реактив модел вьюшка
+   public ReactiveViewModel ReactiveViewModel { get; } = new ReactiveViewModel();
     
 }
